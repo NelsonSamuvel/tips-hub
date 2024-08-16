ec02cd1e01e98a10e2d7dc6e86a7556897f1a476
= <=>
2024-08-16 20:14:21 +0530

    change website icon and title

--- src/features/Tips/SearchBarTips.jsx
+++ src/features/Tips/SearchBarTips.jsx
@@ -4,7 +4,7 @@ const SearchBarTips = ({ searchedTip, dispatch, searchBy }) => {
   return (
     <div className="relative">
       <input
-        className="px-2 py-1 md:px-3 md:py-2 w-[300px] rounded-sm focus:outline-none placeholder:font-normal sm:w-[350px] focus:sm:w-[450px] focus:transition-all duration-100"
+        className="px-2 py-1 md:px-3 md:py-2 w-[300px] rounded-sm focus:outline-none placeholder:font-normal sm:w-[350px] focus:md:w-[450px] focus:transition-w duration-100"
         type="text"
         placeholder="search tips here..."
         value={searchedTip}
@@ -19,7 +19,7 @@ const SearchBarTips = ({ searchedTip, dispatch, searchBy }) => {
           onChange={(e) =>
             dispatch({ type: "setSearchBy", payload: e.target.value })
           }
-          className="rounded-sm text-slate-200 bg-slate-700 border-none px-2 py-1  md:px-2.5 md:py-2 focus:outline-none"
+          className=" text-slate-200 bg-slate-700  px-2 py-1 border-none md:px-2.5 md:py-2 focus:outline-none"
         >
           <option value="any">Any</option>
           <option value="tags">Tags</option>
