d5f9d20e36e0a1adacedec056400a8813df31c76
= <nelsonsamvl@gmail.com>
2024-08-20 17:10:33 +0530

    style layouts

--- src/UI/FormRow.jsx
+++ src/UI/FormRow.jsx
@@ -3,7 +3,7 @@
 function FormRow({children,label,error}) {
   return (
     <div className="flex flex-col lg:flex-row lg:items-center gap-2 mb-6">
-        <label htmlFor={children.props.id} className="basis-1/4 md:text-lg text-slate-200 font-semibold">{label}</label>
+        <label htmlFor={children.props.id} className={`basis-1/4 md:text-lg text-slate-200 font-semibold ${error ? 'self-start' : ''}`}>{label}</label>
         <div className="grow">
         {children}
         {error && <p className="text-red-600 text-sm mt-2">{error}</p>}
